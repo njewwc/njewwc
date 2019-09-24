7071fed1c6759d5719bf67b78d4f7db6a951a736
Cole <coleengelhard1998@yahoo.com>
2019-09-24 17:27:21 -0500

    update

---
+++ TestingAssignment/creatAssignmentPass.py
@@ -0,0 +1,1 @@
+assignmentid = '123456789'
--- TestingAssignment/secondTest.py
+++ TestingAssignment/secondTest.py
@@ -1,1 +1,9 @@
-second test
+from creatAssignmentPass import assignmentid
+def test():
+    if len(str(assignmentid)) == 9:
+        length = 1
+    else:
+        length = 0
+    assert length == 1
+
+
