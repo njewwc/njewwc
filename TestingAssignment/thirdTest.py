a1552460ad911c545c134d07f710159bdad6150f
Cole <coleengelhard1998@yahoo.com>
2019-09-25 14:44:11 -0500

    third test

---
+++ TestingAssignment/thirdTest.py
@@ -0,0 +1,24 @@
+from viewCourseListPass import courseList, courseToBeViewed
+
+
+def test():
+
+    y = 0
+    if courseToBeViewed in courseList:
+        y= 1
+    assert y == 1
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ TestingAssignment/viewCourseListPass.py
@@ -0,0 +1,8 @@
+
+studentId = '123456789'
+
+courseList = [1234,2345,3456,4567]
+
+
+courseToBeViewed = 1234
+
